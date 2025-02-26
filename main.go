a3d172c6de5244a501c59b1f42992d0cd25561b1
Eduardo Kairalla <eduardokairalla123@gmail.com>
2025-02-26 13:45:51 -0300

    Adicionando funcao de escolha de sites para monitorar

--- main.go
+++ main.go
@@ -13,6 +13,19 @@ func main() {
 	fmt.Println("Olá, ", nome)
 	fmt.Println("Este programa está na versão", versao)
 
+	var site_escolha string
+	var sites []string
+
+	fmt.Print("Qual site(s) deseja monitorar? Digite 0 para sair: ")
+	for {
+		fmt.Scan(&site_escolha)
+		if site_escolha != "0" {
+			sites = append(sites, site_escolha)
+		} else {
+			break
+		}
+	}
+
 	for {
 		fmt.Println("1 - Iniciar Monitoramento")
 		fmt.Println("2 - Exibir Logs")
@@ -23,7 +36,7 @@ func main() {
 
 		switch escolha {
 		case 1:
-			monitoramento()
+			monitoramento(sites)
 		case 2:
 			fmt.Println("Exibindo Logs...")
 		case 0:
@@ -36,16 +49,18 @@ func main() {
 
 }
 
-func monitoramento() {
+func monitoramento(sites []string) {
 	fmt.Println("Monitorando...")
-	site := "https://www.alura.com.br"
 
-	resp, _ := http.Get(site)
+	for _, site := range sites {
+		resp, _ := http.Get(site)
 
-	if resp.StatusCode == 200 {
-		fmt.Println("Site:", site, "foi carregado com sucesso!")
-	} else {
-		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+		if resp.StatusCode == 200 {
+			fmt.Println("Site:", site, "foi carregado com sucesso!")
+		} else {
+			fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+		}
 
 	}
+
 }
