6e9912cf4886239d7c1ec799d7db7266758584a3
Eduardo Kairalla <eduardokairalla123@gmail.com>
2025-02-26 14:10:16 -0300

    Aprimorando a Funcao 'Monitoramento' e Criando a Funcao 'testaSite'

--- main.go
+++ main.go
@@ -4,24 +4,28 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
 
-func main() {
-	nome := "Eduardo"
-	versao := 1.1
+const nome = "Eduardo"
+const versao = 1.1
+const quantidade_monitoramento = 5
+const delay = 5
 
+func main() {
 	fmt.Println("Olá, ", nome)
 	fmt.Println("Este programa está na versão", versao)
 
 	var site_escolha string
 	var sites []string
 
-	fmt.Print("Qual site(s) deseja monitorar? Digite 0 para sair: ")
 	for {
+		fmt.Print("Qual site(s) deseja monitorar? Digite 0 para sair: ")
 		fmt.Scan(&site_escolha)
 		if site_escolha != "0" {
 			sites = append(sites, site_escolha)
 		} else {
+			fmt.Println("")
 			break
 		}
 	}
@@ -33,6 +37,7 @@ func main() {
 
 		var escolha int
 		fmt.Scan(&escolha)
+		fmt.Println("")
 
 		switch escolha {
 		case 1:
@@ -51,16 +56,32 @@ func main() {
 
 func monitoramento(sites []string) {
 	fmt.Println("Monitorando...")
+	fmt.Println("")
 
-	for _, site := range sites {
-		resp, _ := http.Get(site)
+	for i := 0; i < quantidade_monitoramento; i++ {
+		fmt.Println("Teste ", i+1, ":")
 
-		if resp.StatusCode == 200 {
-			fmt.Println("Site:", site, "foi carregado com sucesso!")
-		} else {
-			fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+		for i, site := range sites {
+			fmt.Println("Testando site", i+1, ":", site)
+
+			testaSite(site)
+
+			fmt.Println("")
 		}
 
-	}
+		println("------------------------------------")
+		fmt.Println("")
 
+		time.Sleep(delay * time.Second)
+	}
+}
+
+func testaSite(site string) {
+	resp, _ := http.Get(site)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+	}
 }
